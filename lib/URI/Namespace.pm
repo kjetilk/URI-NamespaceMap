c5ee0a5b8cc624d3a615ffe1af2cda09ca9f08c2
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2012-08-27 23:03:22 +0200

    Use URI class

--- lib/URI/Namespace.pm
+++ lib/URI/Namespace.pm
@@ -1,15 +1,16 @@
 package URI::Namespace;
 use Moose;
+use URI;
 
 around BUILDARGS {
     my ($n, $s, @a) = @_;
-    return $s->$n(@_) if @a > 1 || ref(@a) eq 'HASH';
+    return $s->$n(@_) if @a > 1 || ref($a[0]) eq 'HASH';
     return { uri => $a[0] };
 }
 
 has uri => ( 
     is => 'ro', 
-    isa => 'Str', 
+    isa => 'URI', 
     required => 1
 );
 
