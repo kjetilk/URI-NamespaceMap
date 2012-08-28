5c053cc3400cf1c29538e685a4a3827d76d93b70
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2012-08-28 13:41:52 +0200

    add coercion
    add buildargs

--- lib/URI/NamespaceMap.pm
+++ lib/URI/NamespaceMap.pm
@@ -1,9 +1,31 @@
 package URI::NamespaceMap;
 use Moose;
+use Moose::Util::TypeConstraints;
+
+around BUILDARGS => sub {
+  my ($next, $self, @parameters) = @_;
+  return $self->$next(@parameters) if (@parameters > 1);
+  return $self->$next(@parameters) if (exists $parameters[0]->{namespace_map});
+  return { namespace_map => $parameters[0] };
+};
+
+subtype NamespaceMap => as HashRef => where { my $h = $_;  return 1 unless values %$h; return if grep { !blessed $_ } values %$h; return 1 };
+coerce NamespaceMap => from HashRef => via {
+    my $hash = $_;
+    return {
+		map {
+		  my $k = $_;
+		  my $v = $hash->{$_}; 
+		  $k => blessed $v ? $v : URI::Namespace->new($v) 
+		} keys %$hash
+	 };
+};
 
 has namespace_map => (
-							 isa => 'HashRef',
+							 isa => 'NamespaceMap',
 							 traits => ['Hash'],
+
+							 coerce => 1,
 							 default => sub { {} },
 							 handles => {
 											 add_mapping => 'set',
