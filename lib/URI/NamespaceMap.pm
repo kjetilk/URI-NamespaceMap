e64cc3f497574a6ae43fd0c8d2fe0fd7d2255f0a
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2014-05-18 21:58:31 +0200

    bump version

--- lib/URI/NamespaceMap.pm
+++ lib/URI/NamespaceMap.pm
@@ -12,11 +12,11 @@ URI::NamespaceMap - Class holding a collection of namespaces
 
 =head1 VERSION
 
-Version 0.07_1
+Version 0.07_2
 
 =cut
 
-our $VERSION = '0.07_1';
+our $VERSION = '0.07_2';
 
 
 =head1 SYNOPSIS
