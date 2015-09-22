f0dc59dd5c68bfddcc5121dcd32a89257261038c
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2015-09-22 22:11:59 +0200

    Bump version again

--- lib/Types/Namespace.pm
+++ lib/Types/Namespace.pm
@@ -6,7 +6,7 @@ use Type::Library -base, -declare => qw( Uri Iri Namespace NamespaceMap );
 use Types::Standard qw( HashRef InstanceOf );
 use Types::URI qw();
 
-our $VERSION = '0.26_01';
+our $VERSION = '0.27_01';
 
 =head1 NAME
 
