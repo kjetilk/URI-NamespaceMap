befa918cff78c58349f8ed6efe325c33a5b4f788
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2016-02-17 05:02:05 +0100

    bump version

--- lib/Types/Namespace.pm
+++ lib/Types/Namespace.pm
@@ -6,7 +6,7 @@ use Type::Library -base, -declare => qw( Uri Iri Namespace NamespaceMap );
 use Types::Standard qw( HashRef InstanceOf );
 use Types::URI qw();
 
-our $VERSION = '0.31_01';
+our $VERSION = '0.31_02';
 
 =head1 NAME
 
--- lib/URI/Namespace.pm
+++ lib/URI/Namespace.pm
@@ -8,7 +8,7 @@ use IRI 0.003;
 use Types::Namespace 0.004 qw( Iri );
 use namespace::autoclean;
 
-our $VERSION = '0.31_01';
+our $VERSION = '0.31_02';
 
 =head1 NAME
 
