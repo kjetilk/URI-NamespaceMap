134ad9f07da4da89e55fc9176189cc819db67d05
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2017-07-06 16:27:16 +0200

    Special-case only XSD map with regards to hash, see issue #14

--- lib/URI/Namespace.pm
+++ lib/URI/Namespace.pm
@@ -83,8 +83,8 @@ sub iri {
 	my ($self, $name) = @_;
 	if (defined($name)) {
 		my $str = $self->_uri->as_string;
-		my $lastc = substr($str, -1); # Find the last character of the string
-		$str .= '#' unless (($lastc eq '#') or ($lastc eq '/'));
+		# XSD is given without hash in XML world, but hash should be added anyway (see issue #14)
+		$str .= '#' if ($str eq 'http://www.w3.org/2001/XMLSchema');
 		return IRI->new($str . "$name");
 	} else {
 		return $self->_uri;
@@ -95,8 +95,7 @@ sub uri {
 	my ($self, $name) = @_;
 	my $iri = $self->_uri->as_string;
 	if (defined($name)) {
-		my $lastc = substr($iri, -1); # Find the last character of the string
-		$iri .= '#' unless (($lastc eq '#') or ($lastc eq '/')); 
+		$iri .= '#' if ($iri eq 'http://www.w3.org/2001/XMLSchema');
 		return URI->new($iri . "$name");
 	} else {
 		return URI->new($iri);
